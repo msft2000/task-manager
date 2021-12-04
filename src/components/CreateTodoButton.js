4ff4805d6be248740697251fe0827a6ac4c167ca
Mateo Flores <dragonnegro1231@gmail.com>
2021-12-04 08:30:01 -0500

    Correcion errores esteticos

--- src/components/CreateTodoButton.js
+++ src/components/CreateTodoButton.js
@@ -1,16 +1,19 @@
 import React from "react";
 import '../css/CreateTodoButton.css';
+import signomas from "../assets/imgs/signomas1.svg";
 function CreateTodoButton({setOpenModal}){
     const onClickFunction = () => {
         setOpenModal(true);
     }
     return(
-        <button
+        <img
             onClick={onClickFunction}
             className="CreateTodoButton"
+            alt="signo mas"
+            src={signomas}
         >
-            &#10133;
-        </button>
+            {/* &#10133; */}
+        </img>
     );
 }
 export { CreateTodoButton };
